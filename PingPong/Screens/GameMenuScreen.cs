6cf903d78902377b98e4dbcc71ddd1a5bdec93c8
Osinachi <osinachinwagboso@gmail.com>
2024-09-02 15:55:08 +0100

    Game resources are now included inResources.resx instead of game root

--- PingPong/Screens/GameMenuScreen.cs
+++ PingPong/Screens/GameMenuScreen.cs
@@ -7,6 +7,7 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using PingPong.Inerface;
+using PingPong.Properties;
 using PingPong.SimpleSprite;
 
 namespace PingPong.Screens
@@ -25,24 +26,37 @@ namespace PingPong.Screens
         private SpriteFont _menuItemFont;
         private readonly GraphicsDevice _graphicsDevice;
         private Texture2D _menuSelectionTexture;
+        private Texture2D _menuSelection2Texture;
 
-        Vector2 optionOnePosition, 
-            optionTwoPosition, 
-            optionThreePosition, 
-            _selectorPosition,
-            optionOneSize,
-            optionTwoSize,
-            optionThreeSize,
-            titleSize,
-            titlePosition;
+        private Vector2 optionOnePosition;
 
-        string titleName = "Ping Pong";
-        string optionOne = "Player vs Player";
-        string optionTwo = "Player vs Computer";
-        string optionThree = "Computer vs Computer";
+        private Vector2 optionTwoPosition;
+
+        private Vector2 optionThreePosition;
+
+        private Vector2 _selectorPosition;
+        private Vector2 _selector2Position;
+
+        private Vector2 optionOneSize;
+
+        private Vector2 optionTwoSize;
+
+        private Vector2 optionThreeSize;
+
+        private Vector2 titleSize;
+
+        private Vector2 titlePosition;
+
+        string titleName = "Super Pong";
+
+        string optionOne = "Arcade Mode";
+        string optionTwo = "Player vs Player";
+        string optionThree = "Player vs Computer";
 
         public delegate void MenuOptionSelectedHandler(int selectedOption);
         public event MenuOptionSelectedHandler OnMenuOptionSelected;
+
+        private Color SelectedMenuColor { get; set; } = Color.YellowGreen;
 
 
         private int _menuSelection = 1;
@@ -65,10 +79,18 @@ namespace PingPong.Screens
             // Load snowflake texture
             _snowflakeTexture = SnowFlakeTexture.CreateSnowflakeTexture(_graphicsDevice, 8, 8);
 
-            using (FileStream stream = new FileStream("MenuSelector.png", FileMode.Open))
+            // Load menu selection texture from resources
+
+            using (MemoryStream stream = new MemoryStream(Resources.MenuSelector))
             {
                 _menuSelectionTexture = Texture2D.FromStream(_graphicsDevice, stream);
             }
+
+            using (MemoryStream stream = new MemoryStream(Resources.MenuSelector2))
+            {
+                _menuSelection2Texture = Texture2D.FromStream(_graphicsDevice, stream);
+            }
+
 
             _menuTitleFont = contentManager.Load<SpriteFont>("MenuTitleFont");
             _menuItemFont = contentManager.Load<SpriteFont>("MenuItem");
@@ -88,7 +110,9 @@ namespace PingPong.Screens
             optionTwoSize = _menuItemFont.MeasureString(optionTwo);
             optionThreeSize = _menuItemFont.MeasureString(optionThree);
 
-            _selectorPosition = new Vector2((_screenWidth / 2 - (_menuSelectionTexture.Width * 1.5f) / 2) - 150, 200);
+            _selectorPosition = new Vector2((_screenWidth / 2 - (_menuSelectionTexture.Width * 1.5f) / 2) - 200, 200);
+            _selector2Position = new Vector2((_screenWidth / 2 - (_menuSelectionTexture.Width * 1.5f) / 2) + 300, 200);
+
             optionOnePosition = new Vector2(_screenWidth / 2 - optionOneSize.X / 2, 200);
             optionTwoPosition = new Vector2(_screenWidth / 2 - optionTwoSize.X / 2, 250);
             optionThreePosition = new Vector2(_screenWidth / 2 - optionThreeSize.X / 2, 300);
@@ -116,12 +140,14 @@ namespace PingPong.Screens
                 {
                     _menuSelection++;
                     _selectorPosition.Y += 50;
+                    _selector2Position.Y += 50;
                     _timeSinceLastInput = 0; // Reset cooldown
                 }
                 else if (keyboardState.IsKeyDown(Keys.Up) && _menuSelection > 1)
                 {
                     _menuSelection--;
                     _selectorPosition.Y -= 50;
+                    _selector2Position.Y -= 50;
                     _timeSinceLastInput = 0; // Reset cooldown
                 }
                 else if (keyboardState.IsKeyDown(Keys.Enter))
@@ -133,7 +159,7 @@ namespace PingPong.Screens
             
         }
 
-
+        
 
         public async Task Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
@@ -148,7 +174,8 @@ namespace PingPong.Screens
 
             // Draw the menu selector
 
-            spriteBatch.Draw(_menuSelectionTexture, _selectorPosition, null, Color.White, 0f, Vector2.Zero, 0.2f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(_menuSelectionTexture, _selectorPosition, null, Color.White, 0f, Vector2.Zero, 0.5f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(_menuSelection2Texture, _selector2Position, null, Color.White, 0f, Vector2.Zero, 0.5f, SpriteEffects.None, 0f);
 
             // Draw the title at the top center
 
@@ -157,9 +184,14 @@ namespace PingPong.Screens
             // Draw the menu options
 
 
-            spriteBatch.DrawString(_menuItemFont, optionOne, optionOnePosition, Color.White);
-            spriteBatch.DrawString(_menuItemFont, optionTwo, optionTwoPosition, Color.White);
-            spriteBatch.DrawString(_menuItemFont, optionThree, optionThreePosition, Color.White);
+            spriteBatch.DrawString(_menuItemFont, optionOne, optionOnePosition,
+                _menuSelection == 1 ? SelectedMenuColor : Color.White);
+
+            spriteBatch.DrawString(_menuItemFont, optionTwo, optionTwoPosition,
+                _menuSelection == 2 ? SelectedMenuColor : Color.White);
+
+            spriteBatch.DrawString(_menuItemFont, optionThree, optionThreePosition,
+                _menuSelection == 3 ? SelectedMenuColor : Color.White);
         }
 
         public async Task UnloadContent()
